60987c2c15228b1f7e670941156a66b4b5c5e7fa
lalikm <marian.lalik@autocont.sk>
2023-06-29 15:21:41 +0200

    changing cards to use mui components

--- src/extensions/formTemplates/components/cards/textMultiLineCard.tsx
+++ src/extensions/formTemplates/components/cards/textMultiLineCard.tsx
@@ -1,53 +1,50 @@
 import { FormDisplayMode } from '@microsoft/sp-core-library'
 import * as React from 'react'
 import { LocaleStrings } from '../formTemplates'
+import { TextField } from '@mui/material'
 
 interface ITextMultiLineCard {
-    id: string
-    title: string
-    displayMode: FormDisplayMode
-    required: boolean
-    itemHandle: IHandle<string>
-    valueVerify?: (value: string) => string
+  id: string
+  title: string
+  displayMode: FormDisplayMode
+  required: boolean
+  itemHandle: IHandle<string>
+  valueVerify?: (value: string) => string
 }
 
-const TextMultiLineCard: React.FC<ITextMultiLineCard> = ({id, title, displayMode, required, itemHandle, valueVerify = (value) => {return ''}}) => {
-  const [errorMessage, setErrorMessage] = React.useState<string>('')
+const TextMultiLineCard: React.FC<ITextMultiLineCard> = ({id, title, displayMode, required, itemHandle, valueVerify = (value): string => null}) => {
+  const [error, setError] = React.useState<boolean>(itemHandle.value ? false : required)
+  const [errorMessage, setErrorMessage] = React.useState<string>()
 
-  const onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void  = (event) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     itemHandle.setValue(event.target.value)
   }
 
   React.useEffect(() => {
-    if (required && !itemHandle.value) {
-      setErrorMessage(`${LocaleStrings.Cards.PleaseFill} ${title ? title : LocaleStrings.Cards.ThisField}`)
-      return
-    }
-    setErrorMessage(valueVerify(itemHandle.value))
+    const verifyResult = valueVerify(itemHandle.value)
+    const isErrorVal = itemHandle.value ? false : required || verifyResult ? true : false
+    setError(isErrorVal)
+    setErrorMessage(isErrorVal ? (
+      !itemHandle.value && required
+      ? `${LocaleStrings.Cards.PleaseFill} ${title ? title : LocaleStrings.Cards.ThisField}`
+      : verifyResult) : null)
   }, [itemHandle.value, required])
 
   try {
-    return displayMode === FormDisplayMode.Display ? (
-      <div className='card-tall'>
-        <label htmlFor={id} className={`card-label ${required ? 'card-required' : ''}`}>
-          {title}
-        </label>
-        <div id={id} className='card-input-d card-tall-display'>{itemHandle.value}</div>
-      </div>
-    )
-    : (
-      <div className='card-tall'>
-        <label htmlFor={id} className={`card-label ${required ? 'card-required' : ''}`}>
-          {title}
-        </label>
-        <textarea
-          className='card-input card-tall-display'
-          id={id}
-          value={itemHandle.value}
-          onChange={onChange}
-        />
-        {errorMessage && errorMessage !== '' ? <div className='card-error'>{errorMessage}</div> : <></>}
-      </div>
+    return (
+      <TextField
+        id={id}
+        disabled={displayMode === FormDisplayMode.Display}
+        fullWidth
+        
+        label={title}
+        variant='standard'
+        required={required}
+        value={itemHandle.value}
+        onChange={onChange}
+        error={error}
+        helperText={errorMessage}
+      />
     )
   }
   catch (error) {
