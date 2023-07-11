099075df6206bb1ce4a36320e965301ec203304b
lalikm <marian.lalik@autocont.sk>
2023-07-11 09:03:58 +0200

    [|:7

--- src/extensions/formTemplates/components/formTemplates.tsx
+++ src/extensions/formTemplates/components/formTemplates.tsx
@@ -4,24 +4,79 @@ import { FormDisplayMode } from '@microsoft/sp-core-library'
 import { FormCustomizerContext } from '@microsoft/sp-listview-extensibility'
 import { GraphFI } from '@pnp/graph'
 import { SPFI } from '@pnp/sp'
-import '@pnp/sp/site-users/web'
-import '@pnp/sp/site-groups/web'
-import '@pnp/sp/webs'
-import '@pnp/sp/lists'
-import '@pnp/sp/items'
-import '@pnp/sp/fields'
-
-import { ILang, getLangStrings } from '../loc/langHelper'
-import { GetColProps } from '../help/helperFunctions'
-import { Button, Stack, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
-import { TextCard } from './cards'
-//import { localeCurrencies } from '../help/dictionaries'
 
 import './formTemplates.module.css'
 import './cards/cardStyles.css'
-import './dataDisplays/dataDisplayStyles.css'
+//import { localeCurrencies } from '../help/dictionaries'
+
+import BaseForm from './subForms/baseForm'
+
+import { ILang, getLangStrings } from '../loc/langHelper'
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export const LocaleStrings: ILang = getLangStrings('en')
+
+export const FormSubmit = async (
+    sp: SPFI,
+    item: Record<string, any>,
+    listGuid: string,
+    displayMode: FormDisplayMode,
+    setErrorMessage: React.Dispatch<React.SetStateAction<string>>,
+    setShow: React.Dispatch<React.SetStateAction<boolean>>,
+    onSave: (item: {}, etag?: string) => Promise<void>
+  ): Promise<void> => {
+  if (displayMode === FormDisplayMode.Display){
+    setErrorMessage(LocaleStrings.Form.DisplaySubmitError)
+    setShow(true)
+    return
+  }
+  if (document.getElementsByClassName('Mui-error').length > 0){
+    setErrorMessage(LocaleStrings.Form.FormSubmitError)
+    setShow(true)
+    return
+  }
+  let etag: string = ''
+  await sp.web.lists.getById(listGuid).items.getById(item.Id)().then((val) => {
+    etag = val['odata.etag']
+  }).catch((error) => {
+    console.error(error)
+  })
+
+  const submitItem = item
+  const dateFields: string[] = []
+  dateFields.forEach((fieldName) => {
+    if (item[fieldName]) {
+      const newTime1 = new Date(item[fieldName].replace(/([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2})[0-9a-zA-Z:.-]*/, '$1'))
+      submitItem[fieldName] = newTime1.toISOString()
+    }
+  })
+  const peopleFields: string[] = []
+  peopleFields.forEach((fieldName) => {
+    if (!item[`${fieldName}Id`]) {
+      item[`${fieldName}Id`] = []
+    }
+    if (!item[`${fieldName}StringId`]) {
+      item[`${fieldName}StringId`] = []
+    }
+  })
+  const listFields: string[] = []
+  listFields.forEach((fieldName) => {
+    if (!item[`${fieldName}Id`]) {
+      item[`${fieldName}Id`] = []
+    }
+  })
+
+  await onSave(submitItem, etag).catch((error: Error) => {
+    if (error.message.indexOf('The request ETag value') !== -1){
+      setErrorMessage(LocaleStrings.Form.ETagValueError)
+    }
+    else {
+      setErrorMessage(error.message)
+    }
+    setShow(true)
+  })
+}
 
 export interface IFormTemplatesProps {
   context: FormCustomizerContext;
@@ -33,127 +88,16 @@ export interface IFormTemplatesProps {
 }
 
 const FormTemplate: FC<IFormTemplatesProps> = (props) => {
-  //#region TEMPLATE_STATES
-    const [item, setItem] = React.useState<Record<string, any>>(props.displayMode === FormDisplayMode.New ? {} : props.context.item) // eslint-disable-line @typescript-eslint/no-explicit-any
-    const [cols, setCols] = React.useState<IColProps[]>([])
-    const [show, setShow] = React.useState<boolean>(false)
-    const [errorMessage, setErrorMessage] = React.useState<string>('')
-    const [sourcePage, setSourcePage] = React.useState<string>('')
-  //#endregion
-  
-  //#region TEMPLATE_FUNCTIONS
-    const handleSubmit: (event: React.FormEvent<HTMLButtonElement>) => void = async (event) => {
-      if (props.displayMode === FormDisplayMode.Display){
-        setErrorMessage(LocaleStrings.Form.DisplaySubmitError)
-        setShow(true)
-        return
-      }
-      if (document.getElementsByClassName('Mui-error').length > 0){
-        setErrorMessage(LocaleStrings.Form.FormSubmitError)
-        setShow(true)
-        return
-      }
-      let etag: string = ''
-      await props.sp.web.lists.getById(props.context.list.guid.toString()).items.getById(item.Id)().then((val) => {
-        etag = val['odata.etag']
-      }).catch((error) => {
-        console.error(error)
-      })
+  const urlParams = new URLSearchParams(window.location.href)
 
-      await props.onSave(item, etag).catch((error: Error) => {
-        if (error.message.indexOf('The request ETag value') !== -1){
-          setErrorMessage(LocaleStrings.Form.ETagValueError)
-        }
-        else {
-          setErrorMessage(error.message)
-        }
-        setShow(true)
-      })
-    }
-  //#endregion
-
-  //#region ON_LOAD
-    React.useEffect(() => {
-      const urlParams = new URLSearchParams(window.location.href)
-      setSourcePage(urlParams?.get('Source'))
-
-      const removeFields = ['@odata.context', '@odata.editLink', '@odata.metadata', '@odata.etag', '@odata.id', '@odata.type',
-        'OData__ColorTag', 'OData__dlc_DocId', 'OData__dlc_DocIdUrl', 'OData__CopySource', 'OData__UIVersionString',
-        'MediaServiceImageTags', 'MediaServiceOCR']
-  
-      if (props.displayMode !== FormDisplayMode.New ) {
-        const tmpItem = item
-        removeFields.forEach(removeField => {
-          delete tmpItem[removeField]
-        })
-        setItem(tmpItem)
-      }
-    
-      props.sp.web.lists.getById(props.context.list.guid.toString()).fields.filter('Hidden eq false')()
-      .then((fields) => {
-        setCols(fields)
-      })
-      .catch(err => {
-        setShow(true)
-        console.error(err)
-      })
-    }, [props])
-  //#endregion
-
-  //#region FORM_CODE
-    const [TitleProps, TitlePropsSet] = React.useState<IColProps>()
-    React.useEffect(() => {
-      TitlePropsSet(GetColProps('Title', cols))
-    }, [cols])
-
-    const StringValSet = (value: string, valueName: string): void => {
-      setItem({
-        ...item,
-        [valueName]: value,
-      })
-    }
-
-    const TitleHandle = {value: item['Title'], setValue: (value: string) => StringValSet(value,'Title')}
-  //#endregion
+  if (props.displayMode === FormDisplayMode.Edit && +urlParams?.get('FormType') === 1) {
+    return (
+      <BaseForm context={props.context} displayMode={props.displayMode} onSave={props.onSave} onClose={props.onClose} graph={props.graph} sp={props.sp} />
+    )
+  }
 
   return (
-    <>
-      <Dialog
-        open={show}
-        onClose={() => {setShow(false)}}
-      >
-        <DialogTitle>
-          {LocaleStrings.Form.DialogTitleError}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>{errorMessage}</DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => {setShow(false)}}>{LocaleStrings.Buttons.DialogClose}</Button>
-        </DialogActions>
-      </Dialog>
-      <form>
-        <Stack direction='column' spacing={2} sx={{maxWidth: '30rem', margin: '1rem'}}>
-          <Stack direction='column' spacing={2}>
-            <TextCard id='Title' title={TitleProps ? TitleProps.Title : ''} displayMode={props.displayMode}
-                required={TitleProps ? TitleProps.Required : false} itemHandle={TitleHandle}/>
-          </Stack>
-          <Stack direction='row' spacing={2}>
-            {props.displayMode === FormDisplayMode.Display
-                ? <Button variant='contained' size='small' color='warning'
-                  href={`${props.context.pageContext.web.absoluteUrl}/_layouts/15/SPListForm.aspx?PageType=6&List=${props.context.list.guid}&ID=${props.context.itemId}&Source=${sourcePage}`}
-                  >
-                    {LocaleStrings.Buttons.Edit}
-                  </Button>
-                : <Button variant='contained' size='small' color='success' onClick={handleSubmit}>{LocaleStrings.Buttons.Save}</Button>}
-            <Button variant='contained' size='small' color='error' onClick={() => {props.onClose()}}>{LocaleStrings.Buttons.Close}</Button>
-            <Button variant='outlined' size='small' color='info' onClick={async () => {
-              console.log(item)
-            }}>Info</Button>
-          </Stack>
-        </Stack>
-      </form>
-    </>
+    <BaseForm context={props.context} displayMode={props.displayMode} onSave={props.onSave} onClose={props.onClose} graph={props.graph} sp={props.sp} />
   )
 }
 
---
+++ src/extensions/formTemplates/components/subForms/baseForm.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react'
+import { FC } from 'react'
+import { FormDisplayMode } from '@microsoft/sp-core-library'
+import '@pnp/sp/site-users/web'
+import '@pnp/sp/site-groups/web'
+import '@pnp/sp/webs'
+import '@pnp/sp/lists'
+import '@pnp/sp/items'
+import '@pnp/sp/fields'
+
+//import { localeCurrencies } from '../help/dictionaries'
+
+import { GetColProps } from '../../help/helperFunctions'
+import { Button, Stack, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import { TextCard } from '../cards'
+import { FormSubmit, IFormTemplatesProps, LocaleStrings } from '../formTemplates'
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+const BaseForm: FC<IFormTemplatesProps> = (props) => {
+  //#region TEMPLATE_STATES
+    const [item, setItem] = React.useState<Record<string, any>>(props.displayMode === FormDisplayMode.New ? {} : props.context.item)
+    const [cols, setCols] = React.useState<IColProps[]>([])
+    const [show, setShow] = React.useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = React.useState<string>('')
+    const [sourcePage, setSourcePage] = React.useState<string>('')
+  //#endregion
+  
+  //#region TEMPLATE_FUNCTIONS
+    const handleSubmit = async (event: React.FormEvent<HTMLButtonElement>): Promise<void> => {
+      await FormSubmit(props.sp, item, props.context.list.guid.toString(), props.displayMode, setErrorMessage, setShow, props.onSave)
+    }
+  //#endregion
+
+  //#region ON_LOAD
+    React.useEffect(() => {
+      const urlParams = new URLSearchParams(window.location.href)
+      setSourcePage(urlParams?.get('Source'))
+
+      const removeFields = ['@odata.context', '@odata.editLink', '@odata.metadata', '@odata.etag', '@odata.id', '@odata.type',
+        'OData__ColorTag', 'OData__dlc_DocId', 'OData__dlc_DocIdUrl', 'OData__CopySource', 'OData__UIVersionString',
+        'MediaServiceImageTags', 'MediaServiceOCR']
+  
+      if (props.displayMode !== FormDisplayMode.New ) {
+        const tmpItem = item
+        removeFields.forEach(removeField => {
+          delete tmpItem[removeField]
+        })
+        setItem(tmpItem)
+      }
+    
+      props.sp.web.lists.getById(props.context.list.guid.toString()).fields.filter('Hidden eq false')()
+      .then((fields) => {
+        setCols(fields)
+      })
+      .catch(err => {
+        setShow(true)
+        console.error(err)
+      })
+    }, [props])
+  //#endregion
+
+  //#region FORM_CODE
+    const [TitleProps, TitlePropsSet] = React.useState<IColProps>()
+    React.useEffect(() => {
+      TitlePropsSet(GetColProps('Title', cols))
+    }, [cols])
+
+    const StringValSet = (value: string, valueName: string): void => {
+      setItem({
+        ...item,
+        [valueName]: value,
+      })
+    }
+
+    const TitleHandle = {value: item['Title'], setValue: (value: string) => StringValSet(value,'Title')}
+  //#endregion
+
+  return (
+    <>
+      <Dialog
+        open={show}
+        onClose={() => {setShow(false)}}
+      >
+        <DialogTitle>
+          {LocaleStrings.Form.DialogTitleError}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>{errorMessage}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => {setShow(false)}}>{LocaleStrings.Buttons.DialogClose}</Button>
+        </DialogActions>
+      </Dialog>
+      <form>
+        <Stack direction='column' spacing={2} sx={{maxWidth: '30rem', margin: '1rem'}}>
+          <Stack direction='column' spacing={2}>
+            <TextCard id='Title' title={TitleProps ? TitleProps.Title : ''} displayMode={props.displayMode}
+                required={TitleProps ? TitleProps.Required : false} itemHandle={TitleHandle}/>
+          </Stack>
+          <Stack direction='row' spacing={2}>
+            {props.displayMode === FormDisplayMode.Display
+                ? <Button variant='contained' size='small' color='warning'
+                  href={`${props.context.pageContext.web.absoluteUrl}/_layouts/15/SPListForm.aspx?PageType=6&List=${props.context.list.guid}&ID=${props.context.itemId}&Source=${sourcePage}`}
+                  >
+                    {LocaleStrings.Buttons.Edit}
+                  </Button>
+                : <Button variant='contained' size='small' color='success' onClick={handleSubmit}>{LocaleStrings.Buttons.Save}</Button>}
+            <Button variant='contained' size='small' color='error' onClick={() => {props.onClose()}}>{LocaleStrings.Buttons.Close}</Button>
+            <Button variant='outlined' size='small' color='info' onClick={async () => {
+              console.log(item)
+            }}>Info</Button>
+          </Stack>
+        </Stack>
+      </form>
+    </>
+  )
+}
+
+export default BaseForm
